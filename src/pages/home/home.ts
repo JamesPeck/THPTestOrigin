5181c50f92d537294f6dc196c18342736814f941
James Peck <James_Peck@tufts-health.com>
2020-05-14 10:46:53 -0400

    Init

---
+++ src/pages/home/home.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Http, RequestOptions, Headers } from "@angular/http";
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, public http: Http) {
+
+  }
+
+  logout() {
+    return new Promise((resolve, reject) => {
+      var headers = new Headers();
+      var url = 'https://mobilemembers.tufts-health.com/siteminderagent/forms/logout.html';
+
+      headers.append('Content-Type', 'application/json');
+      headers.append('SMSESSION', 'test');
+
+      let options: RequestOptions = new RequestOptions({ headers: headers });
+      this.http.get(url, options)
+          .subscribe(async data => {
+            console.log("Working")
+              resolve(data);
+          },
+              error => {
+                  reject(error);
+              });
+  });
+  }
+
+}
